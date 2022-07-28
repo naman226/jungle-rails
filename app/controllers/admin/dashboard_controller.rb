fc3ece317aebb71c6dcf82296aa1d01d22b1d3f9
vagrant <namandhalla2261994@gmail.com>
2022-07-28 03:28:38 +0000

    Add dashboard query

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,7 +3,9 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],
   password: ENV["HTTP_BASIC_PASSWORD"],
   if: -> { ENV["HTTP_BASIC_PASSWORD"].present? }
-  
+
   def show
+    @products = Product.count
+    @categories = Category.count
   end
 end
