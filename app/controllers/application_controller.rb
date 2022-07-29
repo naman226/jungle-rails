5d6528584c7c7ec85fd9fd71f09c44c16c6dc43b
vagrant <namandhalla2261994@gmail.com>
2022-07-29 02:16:47 +0000

    Add user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :set_current_user
+    def set_current_user
+      # finds user with session data and stores it if present
+      Current.user = User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+    def require_user_logged_in!
+      # allows only logged in user
+      redirect_to sign_in_path, alert: 'You must be signed in' if Current.user.nil?
+    end
+
   private
 
   def cart
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  get 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create', as: 'log_in'
+  delete 'logout', to: 'sessions#destroy'
+
   resources :about, only: [:index] 
 
   resources :products, only: [:index, :show]
